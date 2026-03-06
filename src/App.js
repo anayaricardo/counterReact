a0c7fe6e067034caf89c065a08342f4b113e7f16
Ricardo Anaya <einstein.letsgetrelative@gmail.com>
2026-03-06 01:35:59 -0600

    background update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Contador } from "./Components/Contador/Contador";
 import { Nav } from "./Components/Nav/Nav";
+import doodleWebp from "./img/doodle2.webp";
 
 function App() {
   const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    // Preload improves first paint of the tiled body background.
+    const preloadLink = document.createElement("link");
+    preloadLink.rel = "preload";
+    preloadLink.as = "image";
+    preloadLink.href = doodleWebp;
+    document.head.appendChild(preloadLink);
+
+    return () => {
+      preloadLink.remove();
+    };
+  }, []);
 
   const suma = () => {
     setContador(contador + 1);
